7406df139beaf56591ceb39b8384fe1d7ee7960b
Sean Gilligan <sean@msgilligan.com>
2016-02-07 16:23:37 -0800

    Misc cleanup:
    * Upgrade to Bootstrap 3.0.3
    * Don’t load unused CSS and JavaScript

--- js/main.js
+++ js/main.js
@@ -7,16 +7,6 @@ jQuery(function($) {
 		});
 	});
 
-	//Ajax contact
-	var form = $('.contact-form');
-	form.submit(function () {
-		$this = $(this);
-		$.post($(this).attr('action'), function(data) {
-			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
-		},'json');
-		return false;
-	});
-
 	//smooth scroll
 	$('.navbar-nav > li').click(function(event) {
 		event.preventDefault();
@@ -31,25 +21,4 @@ jQuery(function($) {
 		var $spy = $(this).scrollspy('refresh')
 	})
 
-	//PrettyPhoto
-	$("a.preview").prettyPhoto({
-		social_tools: false
-	});
-
-	//Isotope
-	$(window).load(function(){
-		$portfolio = $('.portfolio-items');
-		$portfolio.isotope({
-			itemSelector : 'li',
-			layoutMode : 'fitRows'
-		});
-		$portfolio_selectors = $('.portfolio-filter >li>a');
-		$portfolio_selectors.on('click', function(){
-			$portfolio_selectors.removeClass('active');
-			$(this).addClass('active');
-			var selector = $(this).attr('data-filter');
-			$portfolio.isotope({ filter: selector });
-			return false;
-		});
-	});
 });
